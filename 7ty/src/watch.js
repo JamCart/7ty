a3274ed1b5d55f861650ca22cf3d5e74f515740b
Brian Keyes <b.keyes@blueglassmoon.com>
2021-03-06 14:18:11 -0600

    Prevent watcher from running multiple times simultaneously

--- 7ty/src/watch.js
+++ 7ty/src/watch.js
@@ -2,15 +2,37 @@ import build from './build.js'
 import chalk from 'chalk'
 import chokidar from 'chokidar'
 
+function asyncDebounce (fn) {
+  let current
+  let repeat
+  
+  return async function debounced (...args) {
+    if (current) {
+      repeat = true
+      return
+    }
+  
+    current = fn(...args)
+    await current
+    current = null
+  
+    if (repeat) {
+      repeat = false
+      debounced()
+    }
+  }
+}
+
+
 export default async function watch () {
   await build({ watch: true })
 
   chokidar.watch(['./static', './src'], {
     ignoreInitial: true
-  }).on('all', () => {
-    build({ watch: true }).catch(e => {
+  }).on('all', asyncDebounce(() => {
+    return build({ watch: true }).catch(e => {
       console.error(e)
       console.log(chalk.red("Build aborted"))
     })
-  })
+  }))
 }
